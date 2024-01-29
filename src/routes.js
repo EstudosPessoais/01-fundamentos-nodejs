85893567621bcae98ca9081740bf03c807c06884
bielb1b2 <gabrielvt.souza@hotmail.com>
2024-01-29 00:29:15 -0300

    feat: rota update and delete

--- src/routes.js
+++ src/routes.js
@@ -15,7 +15,7 @@ export const routes = [
 
             return res
                 .end(JSON.stringify(users));
-        }   
+        }
     },
     {
         method: 'POST',
@@ -29,17 +29,36 @@ export const routes = [
                 name,
                 email,
             })
-    
+
             database.insert('users', user);
-        
+
             return res.writeHead(201).end();
-        }   
+        }
+    },
+    {
+        method: 'PUT',
+        path: buildRoutePath('/users/:id'),
+        handler: (req, res) => {
+            const { id } = req.params;
+            const { name, email } = req.body
+
+            database.update('users', id, {
+                name,
+                email
+            });
+
+            return res.writeHead(204).end();
+        }
     },
     {
         method: 'DELETE',
         path: buildRoutePath('/users/:id'),
         handler: (req, res) => {
-            res.writeHead(202).end();
-        }   
+            const { id } = req.params;
+
+            database.delete('users', id);
+
+            return res.writeHead(204).end();
+        }
     }
 ]
